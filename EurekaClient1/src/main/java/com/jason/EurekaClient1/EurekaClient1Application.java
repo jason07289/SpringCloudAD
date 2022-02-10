d42637b99c94348117c3ff7314b6230a7c6f05b3
jaehyuk0728 <jaehyuk0728@01-04396.nsseshop.com>
2022-02-10 14:24:18 +0900

    hystrix

--- EurekaClient1/src/main/java/com/jason/EurekaClient1/EurekaClient1Application.java
+++ EurekaClient1/src/main/java/com/jason/EurekaClient1/EurekaClient1Application.java
@@ -2,14 +2,24 @@ package com.jason.EurekaClient1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaClient1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClient1Application.class, args);
+	}
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+	return new RestTemplate();
 	}
 
 }
--- EurekaClient1/src/main/java/com/jason/EurekaClient1/controller/SampleController.java
+++ EurekaClient1/src/main/java/com/jason/EurekaClient1/controller/SampleController.java
@@ -1,12 +1,26 @@
 package com.jason.EurekaClient1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@RefreshScope
 @RestController
 public class SampleController {
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@HystrixCommand
 	@RequestMapping("/hello")
-	public String printHelloWorld() {
-	return "Hello MSA World! : 1st Client";
+	public String printHelloWorld() throws InterruptedException {
+		Thread.sleep(1000);
+		String result = restTemplate.getForObject("http://eurekaclient2/hello", String.class);
+
+		return result;
 	}
 }
